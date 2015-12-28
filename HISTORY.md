65a612d7b05d37643bb499b128b743ea6331acca
玄寂 <xuanji.jw@taobao.com>
2015-12-28 22:49:08 +0800

    fix: fix html lacks of doctype

---
+++ HISTORY.md
@@ -0,0 +1,12 @@
+# History
+
+---
+
+## 0.1.1
+
+- fix: fix html lacks of doctype
+
+
+## 0.1.0
+
+- initial version
--- src/index.js
+++ src/index.js
@@ -23,16 +23,19 @@ export default {
       const filePath = join(cwd, fileName);
       const isHTML = /\.html?$/.test(this.url.split('?')[0]);
       if (isHTML && existsSync(filePath)) {
+        const injectScript = '<script src="./dora-plugin-debug-corner.js"></script>';
         let content = readFileSync(filePath, 'utf-8');
         const docTypeReg = new RegExp('^\s*\<\!DOCTYPE\s*.+\>.*$', 'im');
         const docType = content.match(docTypeReg);
         if (docType) {
-          content = content.replace(docTypeReg, docType[0] + '<script src="./dora-plugin-debug-corner.js"></script>');
+          content = content.replace(docTypeReg, docType[0] + injectScript);
           this.body = content;
 
           return;
         }
-        yield next;
+        content = injectScript + content;
+
+        return;
       }
       yield next;
     };
