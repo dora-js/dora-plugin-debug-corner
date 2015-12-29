37ec4422b0a81112ce60c5e096cf3380ba2d40a9
玄寂 <xuanji.jw@taobao.com>
2015-12-29 11:57:15 +0800

    fix: 404 when html file lack of doctype and  add more test cases

--- src/index.js
+++ src/index.js
@@ -34,6 +34,7 @@ export default {
           return;
         }
         content = injectScript + content;
+        this.body = content;
 
         return;
       }
--- test/index-test.js
+++ test/index-test.js
@@ -25,5 +25,38 @@ describe('debug-corner', () => {
         done();
       });
   });
+
+  it('GET /index.html is injected the script dora-plugin-debug-corner.js', done => {
+    request(`http://localhost:${port}`)
+      .get('/index.html')
+      .expect(200)
+      .end(function(err, res){
+        if (err) return done(err);
+        if (res.text.indexOf('<script src="./dora-plugin-debug-corner.js"></script>') < 0) throw new Error("dora-plugin-debug-corner.js is not injected"); 
+        done();
+      });
+  });
+
+  it('GET /lackdoctype.html is injected the script dora-plugin-debug-corner.js', done => {
+    request(`http://localhost:${port}`)
+      .get('/lackdoctype.html')
+      .expect(200)
+      .end(function(err, res){
+        if (err) return done(err);
+        if (res.text.indexOf('<script src="./dora-plugin-debug-corner.js"></script>') < 0) throw new Error("dora-plugin-debug-corner.js is not injected"); 
+        done();
+      });
+  });
+
+  it('GET /index.js should not be handled', done => {
+    request(`http://localhost:${port}`)
+      .get('/index.js')
+      .expect(200)
+      .end(function(err, res){
+        if (err) return done(err);
+        if (res.text.indexOf('console.log(2);') < 0) throw new Error("other types of files should not be handled"); 
+        done();
+      });
+  });
 });
 
